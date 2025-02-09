f53d4b91d530dc3d7e190ad564d56b3d47267901
Tarun Naidu g <117089439+tharunnaidug@users.noreply.github.com>
2025-02-09 15:21:39 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <img src="/logot.png" alt="logo" height={"350px"} />
+      <h1>Welcome to AgriTarde</h1>
+      <p>We will be live soon..!!</p>
+    </>
+  )
+}
+
+export default App
