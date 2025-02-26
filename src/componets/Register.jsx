a7c618ccf3e754f1004c47202044399a0f47976c
Tarun Naidu g <117089439+tharunnaidug@users.noreply.github.com>
2025-02-26 16:25:46 +0530

    Added Pic Upload While registering

--- src/componets/Register.jsx
+++ src/componets/Register.jsx
@@ -26,21 +26,15 @@ const Register = () => {
 
   const handleFileUpload = async (file) => {
     const formData = new FormData();
-    formData.append("file", file);
-    formData.append("upload_preset", "ml_default");
-    formData.append("cloud_name","Ydnpu3oocg")
-
-    try {
-      const response = await fetch(`https://api.cloudinary.com/v1_1/Ydnpu3oocg/image/upload`, {
-        method: "POST",
-        body: formData
-      });
-      const data = await response.json();
-      return data.secure_url;
-    } catch (error) {
-      console.error("Error uploading file:", error);
-      return null;
-    }
+    formData.append("image", file);
+  
+    const response = await fetch(`https://api.imgbb.com/1/upload?key=9abdfc8259278993fe03e52c22e5bada`, {
+      method: "POST",
+      body: formData,
+    });
+  
+    const data = await response.json();
+    return data.data.url; 
   };
 
   const handleFile = async (e) => {
