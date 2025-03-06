5f1517169a134b1fbc6fd5e1b14d0b1978f91ace
Tarun Naidu g <117089439+tharunnaidug@users.noreply.github.com>
2025-03-06 21:56:42 +0530

    Context Added

---
+++ src/context/AppState.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import AppContext from './AppContext'
+
+const AppState = (props) => {
+    return (
+        <AppContext.Provider value={{}}>
+            {props.children}
+        </AppContext.Provider>
+    )
+}
+
+export default AppState
